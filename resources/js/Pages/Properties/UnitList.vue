5b45c5a6c3c03eed6a2541020d2602df080518d0
Jesus Guerrero <jesusant.guerrero@gmail.com>
2023-01-30 05:39:17 -0400

    feat: add pagination to units

--- resources/js/Pages/Properties/UnitList.vue
+++ resources/js/Pages/Properties/UnitList.vue
@@ -2,21 +2,50 @@
 // @ts-ignore: its my template
 import AppLayout from "@/Components/templates/AppLayout.vue";
 import { router } from "@inertiajs/core";
-import { computed } from "vue";
+import { computed, toRefs } from "vue";
 import cols from "./unitCols";
-import AtTable from "@/Components/AtTable.vue";
+import AtTable from "@/Components/shared/BaseTable.vue";
 import AppButton from "@/Components/shared/AppButton.vue";
 import PropertySectionNav from "./Partials/PropertySectionNav.vue";
 import { IPaginatedData } from "@/utils/constants";
 import { IProperty } from "@/Modules/properties/propertyEntity";
+import { useServerSearch } from "@/utils/useServerSearch";
 
 const props = defineProps<{
   units: IProperty[] | IPaginatedData<IProperty>;
+  serverSearchOptions: Object;
 }>();
 
 const listData = computed(() => {
-  return Array.isArray(props.units) ? props.units : props.units.data;
+  return Array.isArray(props.units)
+    ? {
+        data: props.units,
+      }
+    : props.units;
 });
+
+const { serverSearchOptions } = toRefs(props);
+const {
+  executeSearch,
+  updateSearch,
+  changeSize,
+  paginate,
+  state: searchState,
+} = useServerSearch(
+  serverSearchOptions,
+  (finalUrl: string) => {
+    updateSearch(`/units?${finalUrl}`);
+  },
+  {
+    manual: true,
+  }
+);
+
+const tableConfig = {
+  selectable: true,
+  searchBar: true,
+  pagination: true,
+};
 </script>
 
 <template>
@@ -33,9 +62,15 @@ const listData = computed(() => {
 
     <main class="p-5 mx-auto mt-8 text-gray-500 sm:px-6 lg:px-8">
       <AtTable
-        :table-data="listData"
-        :cols="cols"
         class="bg-white rounded-md text-body-1"
+        :table-data="listData.data"
+        :cols="cols"
+        :pagination="searchState"
+        :total="listData.total"
+        @search="executeSearch"
+        @paginate="paginate"
+        @size-change="changeSize"
+        :config="tableConfig"
       />
     </main>
   </AppLayout>
