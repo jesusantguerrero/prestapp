61e5143cd2d7347ca7145094465bf84ff9e6ea82
Jesus Guerrero <jesusant.guerrero@gmail.com>
2023-02-02 14:05:29 -0400

    feat: add job details to client

---
+++ resources/js/Components/shared/AppFormField.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { AtField, AtInput } from "atmosphere-ui";
+
+defineProps<{
+  label: string;
+  modelValue?: any;
+}>();
+</script>
+
+<template>
+  <AtField :label="label" class="w-full text-secondary font-bold">
+    <slot>
+      <AtInput
+        :model-value="modelValue"
+        @update:modelValue="$emit('update:modelValue')"
+        rounded
+        class="bg-neutral/20 shadow-none border-neutral hover:border-secondary/60"
+      />
+    </slot>
+  </AtField>
+</template>
--- resources/js/Pages/Clients/Partials/ClientFormModal.vue
+++ resources/js/Pages/Clients/Partials/ClientFormModal.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
-import { reactive, computed } from "vue";
+import { reactive, computed, ref } from "vue";
+import { router } from "@inertiajs/core";
 // @ts-ignore
-import { AtButton, AtField, AtInput, AtTextarea, AtSimpleSelect } from "atmosphere-ui";
+import { AtButton, AtTextarea, AtSimpleSelect } from "atmosphere-ui";
+
 import Modal from "@/Components/Modal.vue";
+import AppFormField from "@/Components/shared/AppFormField.vue";
+import AppButton from "@/Components/shared/AppButton.vue";
+import SectionNav from "@/Components/SectionNav.vue";
 
 import { clientInteractions } from "@/Modules/clients/clientInteractions";
 import { documentTypes, DOCUMENT_TYPES } from "@/Modules/clients/constants";
-import { router } from "@inertiajs/core";
-import AppButton from "@/Components/shared/AppButton.vue";
 
 const props = defineProps({
   show: {
@@ -41,6 +44,33 @@ const clientForm = reactive({
   address_details: "",
   dni_type: "DNI",
   dni: "",
+  work_name: "",
+  work_email: "",
+  work_cellphone: "",
+  work_address_details: "",
+  bank_name: "",
+  bank_account_number: "",
+  owner_distribution_date: "",
+});
+
+const tabs = [
+  {
+    value: "general",
+    label: "Datos Generales",
+  },
+  {
+    value: "work",
+    label: "Datos de trabajo",
+  },
+  {
+    value: "accounting",
+    label: "Cuenta y Pago",
+  },
+];
+const selectedTab = ref("general");
+
+const currentTab = computed(() => {
+  return tabs.find((tab) => tab.value == selectedTab.value)?.label;
 });
 
 const documentType = computed(() => {
@@ -68,43 +98,85 @@ const onSubmit = () => {
     <header
       class="border-b bg-secondary/80 text-white py-4 px-4 flex items-center justify-between"
     >
-      <h4 class="font-bold text-xl">Crear Contacto {{ type }}</h4>
+      <h4 class="font-bold text-xl">Crear Contacto</h4>
       <button class="hover:text-danger" @click="close()">
         <IMdiClose />
       </button>
     </header>
+    <SectionNav
+      class="bg-primary/10 w-full py-1"
+      v-model="selectedTab"
+      selected-class="bg-base-lvl-3 text-primary font-bold"
+      :sections="tabs"
+    />
     <main class="pb-4 bg-white sm:p-6 sm:pb-4">
-      <section class="flex space-x-2">
-        <AtField label="Nombres" class="w-full">
-          <AtInput
-            v-model="clientForm.names"
+      <h1 class="font-bold text-lg w-full text-center">
+        {{ currentTab }}
+      </h1>
+      <article v-if="selectedTab == 'general'">
+        <section class="flex space-x-2">
+          <AppFormField label="Nombres" v-model="clientForm.names" required />
+          <AppFormField
+            label="Apellidos"
+            v-model="clientForm.lastnames"
             rounded
-            class="bg-neutral/20 shadow-none border-neutral hover:border-secondary/60"
+            required
           />
-        </AtField>
-        <AtField label="Apellidos" class="w-full">
-          <AtInput v-model="clientForm.lastnames" rounded />
-        </AtField>
-      </section>
-      <AtField label="Dirección" class="w-full">
-        <AtTextarea v-model="clientForm.address_details" class="border" />
-      </AtField>
-      <section class="flex space-x-2">
-        <AtField label="Tipo Documento" class="w-full">
-          <AtSimpleSelect :options="documentTypes" v-model="clientForm.dni_type" />
-        </AtField>
-        <AtField :label="documentType" class="w-full">
-          <AtInput v-model="clientForm.dni" rounded />
-        </AtField>
-      </section>
-      <section class="flex space-x-2">
-        <AtField label="Email" class="w-full">
-          <AtInput v-model="clientForm.email" rounded type="email" />
-        </AtField>
-        <AtField label="Telefono" class="w-full">
-          <AtInput v-model="clientForm.cellphone" type="tel" rounded />
-        </AtField>
-      </section>
+        </section>
+        <AppFormField label="Dirección" required>
+          <AtTextarea v-model="clientForm.address_details" class="border" />
+        </AppFormField>
+        <section class="flex space-x-2">
+          <AppFormField label="Tipo Documento" required>
+            <AtSimpleSelect :options="documentTypes" v-model="clientForm.dni_type" />
+          </AppFormField>
+          <AppFormField :label="documentType" v-model="clientForm.dni" rounded required />
+        </section>
+        <section class="flex space-x-2">
+          <AppFormField label="Email" v-model="clientForm.email" rounded type="email" />
+          <AppFormField
+            required
+            label="Telefono"
+            v-model="clientForm.cellphone"
+            type="tel"
+            rounded
+          />
+        </section>
+      </article>
+      <article v-else-if="selectedTab == 'work'">
+        <section class="flex space-x-2">
+          <AppFormField label="Lugar de trabajo" v-model="clientForm.work_name" />
+        </section>
+        <AppFormField label="Dirección de trabajo">
+          <AtTextarea v-model="clientForm.work_address_details" class="border" />
+        </AppFormField>
+        <section class="flex space-x-2">
+          <AppFormField
+            label="Email"
+            v-model="clientForm.work_email"
+            rounded
+            type="email"
+          />
+          <AppFormField
+            label="Telefono"
+            v-model="clientForm.work_cellphone"
+            type="tel"
+            rounded
+          />
+        </section>
+      </article>
+      <article v-else-if="selectedTab == 'accounting'">
+        <section class="flex space-x-2">
+          <AppFormField label="Banco" v-model="clientForm.bank_name" />
+          <AppFormField label="No. de cuenta" v-model="clientForm.bank_account_number" />
+        </section>
+        <section class="flex space-x-2">
+          <AppFormField
+            label="Dia de generacion de factura"
+            v-model="clientForm.owner_distribution_date"
+          />
+        </section>
+      </article>
     </main>
     <footer class="px-6 py-4 space-x-3 text-gray-600 text-right bg-neutral">
       <AtButton @click="close()" class="text-gray"> Cancelar </AtButton>
