c2acbef4757f8dc6d961008709c4c23c97e7ced0
Jesus Guerrero <jesusant.guerrero@gmail.com>
2023-03-21 10:26:18 -0400

    fix: listen for impersonation events

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -5,6 +5,9 @@ namespace App\Providers;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Event;
+use Lab404\Impersonate\Events\LeaveImpersonation;
+use Lab404\Impersonate\Events\TakeImpersonation;
 use Insane\Journal\Listeners\CreateTeamAccounts;
 use Laravel\Jetstream\Events\TeamCreated;
 
@@ -21,7 +24,7 @@ class EventServiceProvider extends ServiceProvider
         ],
         TeamCreated::class => [
             CreateTeamAccounts::class,
-        ]
+        ],
     ];
 
     /**
@@ -31,7 +34,17 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+      Event::listen(TakeImpersonation::class, fn() => $this->clearAuthHashes());
+      Event::listen(LeaveImpersonation::class, fn() => $this->clearAuthHashes());
+    }
+
+    private function clearAuthHashes() {
+      session()->forget(array_unique(
+        [
+          'password_hash_sanctum',
+          'password_hash_web',
+          'password_hash_'. session('impersonate.guard'),
+        ]));
     }
 
     /**
