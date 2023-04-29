5e785cb9e3f54970deec685c41be111af4704b9a
Jesus Guerrero <jesusant.guerrero@gmail.com>
2023-04-29 13:27:33 -0400

    fix: improve rent invoice widget

--- resources/js/Pages/Dashboard/Partials/PropertyInvoiceWidget.vue
+++ resources/js/Pages/Dashboard/Partials/PropertyInvoiceWidget.vue
@@ -2,23 +2,65 @@
 import SectionNav from "@/Components/SectionNav.vue";
 import InvoiceCard from "@/Components/templates/InvoiceCard.vue";
 import NextPaymentsWidget from "@/Pages/Loans/NextPaymentsWidget.vue";
-import { ref } from "vue";
+import { formatDate } from "@/utils";
+import { endOfMonth, startOfMonth } from "date-fns";
+import { ref, computed } from "vue";
+import { useI18n } from "vue-i18n";
 
-const selectedTab = ref("next");
+const { t } = useI18n();
+const selectedTab = ref("month");
 const tabs = {
+  month: {
+    label: t("This month invoices"),
+  },
   next: {
-    label: "Proximas facturas",
+    label: t("Next invoices"),
   },
   overdue: {
-    label: "Facturas vencidas",
+    label: t("Overdue invoices"),
   },
 };
+
+const monthEndpoint = computed(() => {
+  return `/api/invoices?filter[due_date]=${formatDate(
+    startOfMonth(new Date()),
+    "yyyy-MM-dd"
+  )}~${formatDate(endOfMonth(new Date()), "yyyy-MM-dd")}&sort=due_date`;
+});
 </script>
 
 <template>
   <section class="rounded-md overflow-hidden">
     <NextPaymentsWidget
-      v-if="selectedTab == 'next'"
+      v-if="selectedTab == 'month'"
+      :endpoint="monthEndpoint"
+      :ranges="[]"
+    >
+      <template #title>
+        <SectionNav
+          class="bg-base-lvl-3 w-full"
+          selected-class="border-primary font-bold text-primary"
+          v-model="selectedTab"
+          :sections="tabs"
+        />
+      </template>
+      <template v-slot:content="{ list: nextInvoices }">
+        <article class="py-4 space-y-4 my-2 h-[380px] overflow-auto ic-scroller">
+          <template v-if="nextInvoices.length">
+            <InvoiceCard v-for="invoice in nextInvoices" :invoice="invoice" />
+          </template>
+          <section
+            v-else
+            class="flex text-body-1 flex-col justify-center items-center w-full"
+          >
+            <IMdiNoteOff class="text-8xl" />
+            <p class="mt-8">{{ $t("There's no invoices for this month") }}</p>
+          </section>
+        </article>
+      </template>
+    </NextPaymentsWidget>
+    <NextPaymentsWidget
+      v-else-if="selectedTab == 'next'"
       endpoint="/api/invoices?filter[status]=~paid&"
     >
       <template #title>
@@ -47,7 +89,7 @@ const tabs = {
     <NextPaymentsWidget
       v-else
       title="Cuotas atrasadas"
-      endpoint="/api/invoices?filter[status]=overdue"
+      endpoint="/api/invoices?filter[status]=overdue&sort=-due_date"
       method="back"
       default-range="All"
       date-field="payment_date"
@@ -66,6 +108,20 @@ const tabs = {
           v-model="selectedTab"
           :sections="tabs"
         />
+      </template>
+      <template v-slot:content="{ list: nextInvoices }">
+        <article class="py-4 space-y-4 my-2 h-[380px] overflow-auto ic-scroller">
+          <template v-if="nextInvoices.length">
+            <InvoiceCard v-for="invoice in nextInvoices" :invoice="invoice" />
+          </template>
+          <section
+            v-else
+            class="flex text-body-1 flex-col justify-center items-center w-full"
+          >
+            <IMdiNoteOff class="text-8xl" />
+            <p class="mt-8">No hay pagos realizados en este rango de fechas</p>
+          </section>
+        </article>
       </template>
     </NextPaymentsWidget>
   </section>
