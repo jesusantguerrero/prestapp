c715d4dc258fcf50d7e1426d4eef255c27d53235
Jesus Guerrero <jesusant.guerrero@gmail.com>
2023-06-11 18:33:56 -0400

    refactor: improve agent tools

---
+++ app/Domains/Properties/Http/Controllers/RentAgentController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Domains\Properties\Http\Controllers;
+
+use App\Domains\CRM\Services\ClientService;
+use App\Domains\Properties\Services\RentService;
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\Traits\HasEnrichedRequest;
+
+class RentAgentController extends Controller
+{
+    use HasEnrichedRequest;
+
+    public function ___invoke(string $viewName) {
+      dd($viewName);
+      $teamId = request()->user()->current_team_id;
+      $filters = request()->query('filters');
+      $ownerId = $filters['owner'] ?? null;
+
+      $methods = [
+        "owner-draws" => fn() => ClientService::invoices($teamId, $ownerId)->get(),
+        "commissions" => fn() => RentService::commissions($teamId)->get()
+      ];
+
+      $method = $methods[$viewName];
+      $invoices = $method();
+
+      return inertia("RentAgent/Browse",
+      [
+          'invoices' => $invoices,
+          'outstanding' => $invoices->sum('debt'),
+          'paid' => $invoices->sum(function ($invoice) {
+            return $invoice->total - $invoice->debt;
+          }),
+          'owners' => $invoices->map(function($invoice) {
+            return [
+              "value" => $invoice->client_id,
+              "label" => $invoice->client_name,
+            ];
+          })->unique('value')->values(),
+          'properties' => $invoices->map(function($invoice) {
+              return [
+                "value" => $invoice->client_id,
+                "label" => $invoice->client_name,
+              ];
+          })
+      ]);
+    }
+  }
---
+++ resources/js/Pages/Journal/Invoices/Preview.vue
@@ -0,0 +1,78 @@
+<script setup lang="ts">
+import InvoiceSimple from "./printTemplates/Simple.vue";
+
+import { onMounted } from "vue";
+import { IInvoiceWithRelations } from "@/Modules/invoicing/entities";
+
+withDefaults(
+  defineProps<{
+    user: Record<string, string>;
+    invoice: IInvoiceWithRelations;
+    businessData: Record<string, string>;
+    type: string;
+    invoiceTemplate: string;
+  }>(),
+  {
+    invoiceTemplate: "invoice-simple",
+  }
+);
+
+onMounted(() => {
+  // print();
+});
+</script>
+
+<template>
+  <div class="preview-container">
+    <InvoiceSimple
+      :user="user"
+      :type="type"
+      :business-data="businessData"
+      :invoice-data="invoice"
+      id="invoice-content"
+    />
+  </div>
+</template>
+
+<style lang="scss">
+body {
+  background: #333 !important;
+}
+
+.preview-container {
+  background: #333 !important;
+  .simple-template {
+    background-color: #333;
+  }
+  .section-body {
+    background: white;
+    max-width: 210mm;
+    min-height: 265mm;
+    width: 100%;
+    height: auto;
+    display: block;
+    font-size: 12px;
+  }
+
+  tbody,
+  .client-card {
+    font-size: 12px;
+  }
+}
+
+@media print {
+  body {
+    background: white !important;
+  }
+
+  .preview-container {
+    background: white !important;
+    .simple-template {
+      background-color: white;
+    }
+    .section-body {
+      background: white;
+    }
+  }
+}
+</style>
