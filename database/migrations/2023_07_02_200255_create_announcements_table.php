15b9182b9e20bff62089002c279f422c9d9ba796
Jesus Guerrero <jesusant.guerrero@gmail.com>
2023-07-02 21:12:02 -0400

    WIP: add test broadcasting

---
+++ database/migrations/2023_07_02_200255_create_announcements_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('announcements', function (Blueprint $table) {
+          $table->id();
+			    $table->string('title')->default('');
+			    $table->string('description');
+			    $table->text('body');
+          $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('announcements');
+    }
+};
