18843cde7df2b15acda81224ace74f09af84a98a
Jesus Guerrero <jesusant.guerrero@gmail.com>
2024-12-31 19:39:45 -0400

    fix: fix client link in search

--- resources/js/Pages/Clients/Partials/ClientProfile.ts
+++ resources/js/Pages/Clients/Partials/ClientProfile.ts
@@ -36,7 +36,7 @@ export const ClientProfile = defineComponent({
       return h('div', { class: 'flex items-center space-x-2 px-4' }, [
         h(ElAvatar, { shape: 'circle', width: 20, height: 20, maxWidth: 20, maxHeight: 20 }, this.initials),
         h('div', { class: 'ml-2 w-full text-left'},  [
-          h(Link, {class: 'font-bold text-primary', href: getClientLink(this.client)}, this.name ?? this.client?.fullName),
+          h(Link, {class: 'font-bold text-primary', href: getClientLink(this.client, this.type)}, this.name ?? this.client?.fullName),
           h('p', { class: 'text-body-1/80 text-sm'}, this.client?.dni)
         ]),
     ]);
